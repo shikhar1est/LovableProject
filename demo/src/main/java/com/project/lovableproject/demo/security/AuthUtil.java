dbe604bd4be8441fb2ff410b4f4800692cf09e8f
Shikhar Rai <shikharr521@gmail.com>
2026-04-12 00:22:26 +0530

    Custom changes in the default Spring Secuirity Configuration

---
+++ demo/src/main/java/com/project/lovableproject/demo/security/AuthUtil.java
@@ -0,0 +1,23 @@
+package com.project.lovableproject.demo.security;
+
+import com.project.lovableproject.demo.entity.User;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class AuthUtil {
+    @Value("${jwt.secret-key}")
+    private String jwtSecretKey;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(User user){
+
+    }
+}
